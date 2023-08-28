1cab27d06df67cc3ebb0214b5f9e2a0babe93919
Nascode23 <134618481+Nascode23@users.noreply.github.com>
2023-08-28 12:13:42 -0400

    second commit

---
+++ src/utilities/user-functions.js
@@ -0,0 +1,13 @@
+// use rcc
+import axios from "axios";
+// utilities folder is for universal, reusable function
+
+export const signUp = async (FormData)  => {
+
+}
+  render() {
+    return (
+      <div>user-functions</div>
+    )
+  }
+
